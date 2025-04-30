fa9155b9714151dbfac16242994a02522196bd62
Kuldeep Saini <kuldeep@askmetechnologies.com>
2025-04-30 10:42:08 +0530

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProductController;
+
+Route::resource('/', ProductController::class);
+Route::resource('products', ProductController::class);
